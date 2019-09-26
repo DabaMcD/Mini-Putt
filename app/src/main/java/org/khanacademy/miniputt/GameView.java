f6a91ba16ff972ec951b6fc24c69e6cc4065031f
gooberbobber <dabamcdoodle@gmail.com>
2019-09-26 14:25:24 -0300

    Make a black background

--- app/src/main/java/org/khanacademy/miniputt/GameView.java
+++ app/src/main/java/org/khanacademy/miniputt/GameView.java
@@ -2,12 +2,15 @@ package org.khanacademy.miniputt;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 
 public class GameView extends View {
+    private Paint paint = new Paint();
     public GameView(Context context) {
         super(context);
     }
@@ -19,7 +22,9 @@ public class GameView extends View {
     }
     @Override
     protected void onDraw(Canvas canvas) {
-
+        paint.setColor(Color.BLACK);
+        canvas.drawPaint(paint);
+        GameCode.draw(canvas);
 
         super.onDraw(canvas);
     }
