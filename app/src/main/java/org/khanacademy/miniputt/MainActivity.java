32b8d9beea71957eae13b76d1b9b7a14732ccc42
gooberbobber <dabamcdoodle@gmail.com>
2019-09-26 13:54:29 -0300

    wip

--- app/src/main/java/org/khanacademy/miniputt/MainActivity.java
+++ app/src/main/java/org/khanacademy/miniputt/MainActivity.java
@@ -5,10 +5,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private Thread thread;
+    private GameView gameView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        gameView = findViewById(R.id.gameView);
+
+        startMainThread();
+    }
+    private void startMainThread() {
+        thread = new Thread() {
+            public void run() {
+                while (true) {
+                    long previousMillis = System.currentTimeMillis();
+                    try {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                gameView.draw();
+                            }
+                        });
+                        Thread.sleep(Math.abs(previousMillis - System.currentTimeMillis() + 30));
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        thread.start();
     }
 }
---
+++ app/src/main/java/org/khanacademy/miniputt/Screen.java
@@ -0,0 +1,6 @@
+package org.khanacademy.miniputt;
+
+public class Screen {
+    public static int width;
+    public static int height;
+}
