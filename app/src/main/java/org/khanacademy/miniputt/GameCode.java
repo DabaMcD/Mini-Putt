c3461ca146acd68133155592438d50d46fa6129e
gooberbobber <dabamcdoodle@gmail.com>
2019-09-26 14:30:46 -0300

    Getting ready for the PJS code...

--- app/src/main/java/org/khanacademy/miniputt/GameCode.java
+++ app/src/main/java/org/khanacademy/miniputt/GameCode.java
@@ -1,6 +1,7 @@
 package org.khanacademy.miniputt;
 
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 
 class GameCode {
@@ -8,6 +9,19 @@ class GameCode {
     private static Canvas canvas;
     static void draw(Canvas c) {
         canvas = c;
-    }
 
+        pjsCode();
+    }
+    static void rect(float x, float y, float w, float h) {
+        canvas.drawRect(x, y, x + w, y + h, paint);
+    }
+    static void fill(int r, int g, int b) {
+        paint.setColor(Color.rgb(r, g, b));
+    }
+    static void fill(int shade) {
+        paint.setColor(Color.rgb(shade, shade, shade));
+    }
+    static void pjsCode() {
+
+    }
 }
